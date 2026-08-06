b6f4b6f6d9b71c34e11f4072150fd661f934ddb9
Zoe Lederman <ledermanzoe@gmail.com>
2026-08-06 11:00:49 -0700

    break scripts apart

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,6 +60,11 @@ resource "aws_iam_role_policy" "ecr_pull" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.ec2_ecr.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
 resource "aws_iam_instance_profile" "ec2_ecr" {
   name = "snippets-ec2-ecr-profile"
   role = aws_iam_role.ec2_ecr.name
